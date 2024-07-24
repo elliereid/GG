0f19ef8a7369e5a431bae7f8ca6ec8a21b4f2bc6
elliereid <escrypto19@gmail.com>
2024-07-24 12:06:17 -0400

    Add files via upload

---
+++ contracts/SeventeenSeventySix.sol
@@ -0,0 +1,25 @@
+
+// SPDX-License-Identifier: Apache-2.0
+pragma solidity ^0.8.0;
+
+import "@thirdweb-dev/contracts/base/ERC1155SignatureMint.sol";
+
+contract SeventeenSeventySix is ERC1155SignatureMint {
+    constructor(
+        address _defaultAdmin,
+        string memory _name,
+        string memory _symbol,
+        address _royaltyRecipient,
+        uint128 _royaltyBps,
+        address _primarySaleRecipient
+    )
+        ERC1155SignatureMint(
+            _defaultAdmin,
+            _name,
+            _symbol,
+            _royaltyRecipient,
+            _royaltyBps,
+            _primarySaleRecipient
+        )
+    {}
+}
